78a3ded652545b400a8e2c36f3cbd269d690bc6a
hakan <imparatorahmett@gmail.com>
2022-03-19 13:59:52 +0300

    implement shorten frontend

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/shorten', function(req, res) {
   const url = req.body.url;
-
+  console.log(url);
   // add database logic 
 
   res.send({ url: url, short: "1qa3YZX"});
