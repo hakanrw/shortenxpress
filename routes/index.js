7bbeced879a6523a8323131dfc313f32124a883e
hakan <imparatorahmett@gmail.com>
2022-03-19 13:32:18 +0300

    update interface

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/shorten', function(req, res) {
+  const url = req.body.url;
+
+  // add database logic 
+
+  res.send({ url: url, short: "1qa3YZX"});
 });
-
-module.exports = router;
